2ef5b5151dd4063da610b99115351df5504eb0b9
Damien Seguin <dmnsgn@gmail.com>
2021-04-16 15:45:46 +0100

    docs: fix options types

--- types.js
+++ types.js
@@ -2,8 +2,8 @@
  * @typedef {Object} CanvasScreenshotOptions Options for canvas screenshot. All optional.
  * @property {string} [filename=Screen Shot YYYY-MM-DD at HH.MM.SS.png] File name.
  * @property {number} [quality=1] Quality between 0 and 1.
- * @property {boolean} [useBlob="undefined"] Use `canvas.toBlob`.
- * @property {boolean} [download=trye] Automatically download the screenshot.
+ * @property {boolean} [useBlob=undefined] Use `canvas.toBlob`.
+ * @property {boolean} [download=true] Automatically download the screenshot.
  */
 
 export {};
